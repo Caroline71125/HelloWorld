a5445819fce52b72ca9f9cd8ca903d7a05d622a9
Caroline71125 <37354228+Caroline71125@users.noreply.github.com>
2018-03-19 18:22:41 +0800

    Update Dell_crawler.py

--- Dell_crawler.py
+++ Dell_crawler.py
@@ -13,17 +13,57 @@ videocard = driver.find_element_by_xpath('//div[@data-refiner="5712"]')
 videocard.click() #點Videocards下拉選單
 
 vccheckbox = driver.find_element_by_id('6149')
+print(vccheckbox.text, "系列")
 vccheckbox.click() #點Videocards的選項
-time.sleep(5) #選擇要的顯卡,等一下網頁
+time.sleep(5) #等一下網頁
 
+test = {}
+titlelist = list()
 titles = driver.find_elements_by_xpath('//span[@data-testid="configItemProductTitle"]')
 for title in titles:
+    titlelist.append(title.text)
+    #test['title'] = title.text
     print (title.text)
 
+
+pricelist = list()
 prices = driver.find_elements_by_xpath('//strong[@data-testid="psDellPrice"]')
 for price in prices:
+    pricelist.append(price.text)
+    #test['price'] = price.text
     print (price.text)
 
+displaysizelist = list()
+displaysizes = driver.find_elements_by_css_selector('span.text-gray-dark')
+for displaysize in displaysizes:
+    displaysizelist.append(displaysize.text)
+    print (displaysize.text)
+
+
+print(titlelist)
+print(pricelist)
+print(displaysizelist)
+
+
+test['title'] = titlelist
+test['price'] = pricelist
+test['displaysize'] = displaysizelist
+
+print(test)
+
+#vctype = [6149, 6151, 6153, 6157, 6159]
+
+
+
+#class Product():
+# def __init__(self, title, price, displaysize):
+#  self.title = title
+#  self.price = price
+#  self.displaysizes = displaysizes
+#a = Product("")  #建立一個名叫dog的Animal實體(物件)
+#print(a.name)
+
+#//*[@id="ProductStackContainer"]/div[1]/div/div[4]/div[2]/div[1]/div/div/p/span
 
 
 ## splinter
